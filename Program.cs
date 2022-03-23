31e1ef4691d129290bc9049a11d0b4e95c76eca1
GGKTECH\jagadish.pasumarthy <jagadish.pasumarthy@acsicorp.com>
2022-03-23 22:05:50 +0530

    consoleapp

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using ClassLibrary2;
+using System;
+
+namespace ConsoleApp8
+
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int Firstnumber = Convert.ToInt32(Console.ReadLine());
+            int Secondnumber = Convert.ToInt32(Console.ReadLine());
+
+        Adittion k = new Adittion();
+        AddingFirstnameandSecondname k1 = new AddingFirstnameandSecondname();
+         string Firstname = Console.ReadLine();
+         string Secondname = Console.ReadLine();
+            Console.WriteLine(k.Addingtwonumbers(Firstnumber, Secondnumber));
+
+            Console.WriteLine(k1.Fullname(Firstname, Secondname));
+        }
+    }
+
+
+
+
+
+
+    
+       
+}
